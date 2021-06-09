e4c8f56682f73f6f3a7f54ad1d47db64fe674937
rolfel <71531600+rolfel@users.noreply.github.com>
2021-06-08 18:14:33 -0700

    Update map.js

--- panda/js/map.js
+++ panda/js/map.js
@@ -1,12 +1,11 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibHVjeXJvbGZlNDIxIiwiYSI6ImNrbzM4Nzl0YzBjZjAyb21saGQza3BqMnUifQ.T7z70s8jWux0miBjZpajOQ'; //insert your API key here
+mapboxgl.accessToken = 'pk.eyJ1IjoibHVjeXJvbGZlNDIxIiwiYSI6ImNrbzM4Nzl0YzBjZjAyb21saGQza3BqMnUifQ.T7z70s8jWux0miBjZpajOQ'; 
       var map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-120.548843, 476.994110],
       zoom: 20
       });
-      
-      // Add the search bar control to the map.
+
       
 
 map.addControl(
