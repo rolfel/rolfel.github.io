d1044fc60152668c1e1c4fe773e2ae8fdf9ba627
rolfel <71531600+rolfel@users.noreply.github.com>
2021-06-08 18:09:44 -0700

    Update map.js

--- panda/js/map.js
+++ panda/js/map.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibHVjeXJvbGZlNDIxIiwiYSI6ImNrbzM4Nzl0YzBjZjAyb
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-120.548843, 476.994110],
-      zoom: 16
+      zoom: 18
       });
       
       // Add the search bar control to the map.
